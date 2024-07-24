e3c4598048e6821f5dc3432cc28b5f55aa76ef16
sugarvoid <gamedev@kitargo.com>
2024-07-24 16:41:01 -0500

    Remove math from o2_bar draw function

--- src/o2_bar.lua
+++ src/o2_bar.lua
@@ -2,14 +2,15 @@
 OxygenBar = {}
 OxygenBar.__index = OxygenBar
 
-local _oxygen_bar = nil
+
 local O2_BAR_BG = love.graphics.newImage("asset/image/o2_bar.png")
 local MAX = 60
-local POSITION = {20,5}
-local bar_x = POSITION[1] + 3
-local top={}
-local middle=nil
-local bottom=nil
+local BASE_POSITION = {20,5}
+local bar_x = BASE_POSITION[1] + 3
+
+local top=      {x=BASE_POSITION[1] + 3, y=BASE_POSITION[2] + 2.5}
+local middle=   {x=BASE_POSITION[1] + 3, y=BASE_POSITION[2] + 3.5}
+local bottom=   {x=BASE_POSITION[1] + 3, y=BASE_POSITION[2] + 4.5}
 
 function OxygenBar:new()
     local _oxygen_bar = setmetatable({}, OxygenBar)
@@ -21,16 +22,14 @@ function OxygenBar:update()
 
 end
 
-
-
 function OxygenBar:draw()
-     love.graphics.draw(O2_BAR_BG, POSITION[1], POSITION[2])
-        love.graphics.push("all")
-        change_draw_color("#ffffff")
-        love.graphics.line( POSITION[1] + 3, POSITION[2]+2.5, POSITION[1] + 3 + self.value, POSITION[2]+2.5)
-        change_draw_color("#0ce6f2")
-        love.graphics.line( POSITION[1] + 3, POSITION[2]+3.5, POSITION[1] + 3 + self.value, POSITION[2]+3.5)
-        change_draw_color("#0098db")
-        love.graphics.line( POSITION[1] + 3, POSITION[2]+4.5, POSITION[1] + 3 + self.value, POSITION[2]+4.5)
-        love.graphics.pop()
+    love.graphics.draw(O2_BAR_BG, BASE_POSITION[1], BASE_POSITION[2])
+    love.graphics.push("all")
+    change_draw_color("#ffffff")
+    love.graphics.line(top.x, top.y, top.x + self.value, top.y)
+    change_draw_color("#0ce6f2")
+    love.graphics.line(middle.x, middle.y, middle.x + self.value, middle.y)
+    change_draw_color("#0098db")
+    love.graphics.line(bottom.x, bottom.y, bottom.x + self.value, bottom.y)
+    love.graphics.pop()
 end
